fe9d023273acb30a46aa30866bc791825e34cd0e
sajjad ghorbani <sajjadr2001@gmail.com>
2022-05-24 17:20:03 +0430

    add models

---
+++ OurSite.DataLayer/Entities/Access/Role.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using OurSite.DataLayer.Entities.BaseEntities;
+
+namespace OurSite.DataLayer.Entities.Access
+{
+	public class Role : BaseEntity
+    {
+        #region Properties
+        [DisplayName("عنوان نقش")]
+        [MaxLength(50, ErrorMessage = " عنوان نقش نمی‌تواند بیش از {0} کاراکتر باشد")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage ="این فیلد اجباری است")]
+        [DisplayName("نام سیستمی")]
+        [MaxLength(50, ErrorMessage = " نام سیستمی نمی‌تواند بیش از {0} کاراکتر باشد")]
+        public string Name { get; set; }
+        #endregion
+
+
+        #region Realations
+        public ICollection<AccounInRole> AccounInRoles { get; set; }
+        #endregion
+
+    }
+}
+
---
+++ OurSite.DataLayer/Entities/BaseEntities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OurSite.DataLayer.Entities.BaseEntities
+{
+	public class BaseEntity
+	{
+        [Key]
+        public long Id { get; set; }
+
+
+        
+        public DateTime CreateDate { get; set; }
+        public bool IsRemove { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
+
