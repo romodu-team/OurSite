3a9911b632e603395c1c5ddcebe93932a4080d52
sajjad ghorbani <sajjadr2001@gmail.com>
2022-05-28 17:43:26 +0430

    path tools

---
+++ OurSite.Core/Utilities/PathTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurSite.Core.Utilities
+{
+    public static class PathTools
+    {
+        public const string Domain = "https://localhost:7181";
+    }
+}
--- OurSite.WebApi/Program.cs
+++ OurSite.WebApi/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OurSite.Core.Utilities;
 using OurSite.DataLayer.Contexts;
 using OurSite.DataLayer.Interfaces;
 using OurSite.DataLayer.Repositories;
@@ -15,6 +18,19 @@ builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(b
 #region addservices
 builder.Services.AddScoped(typeof(IGenericReopsitories<>), typeof(GenericRepositories<>));
 #endregion
+
+#region Autentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+options.TokenValidationParameters = new TokenValidationParameters()
+{
+    ValidateIssuer = true,
+    ValidateAudience = false,
+    ValidateLifetime = true,
+    ValidIssuer = PathTools.Domain,
+    ValidateIssuerSigningKey = true,
+
+});
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,7 +41,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
