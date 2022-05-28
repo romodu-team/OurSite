1467a0d240bc7de8925b054ef78069dbe552593b
Hanieh Ghasmie <haniehghsmie@gmail.com>
2022-05-28 17:59:18 +0430

    Update Program.cs

--- OurSite.WebApi/Program.cs
+++ OurSite.WebApi/Program.cs
@@ -5,7 +5,7 @@ using OurSite.DataLayer.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
