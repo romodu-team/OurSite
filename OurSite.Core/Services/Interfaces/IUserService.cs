68b00818756eb616cbf164eb4d960cd71a5f75fe
Hanieh Ghasmie <haniehghsmie@gmail.com>
2022-06-04 22:55:08 +0430

    end of user service and controller

--- OurSite.Core/Services/Interfaces/IUserService.cs
+++ OurSite.Core/Services/Interfaces/IUserService.cs
@@ -23,7 +23,7 @@ namespace OurSite.Core.Services.Interfaces
         Task<bool> UpDate(ReqUpdateUserDto userdto);
         Task<RessingupDto> SingUp(ReqSingupUserDto userDto);
 
-        Task<User>ViewProfile(long id);
+        Task<ReqViewUserDto> ViewProfile(long id);
         Task<bool> ChangeUserStatus(long userId);
 
     }
