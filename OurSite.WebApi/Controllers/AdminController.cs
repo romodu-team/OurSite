0b62afb767ac18647de1189c09a1aacd19b88423
Hanieh Ghasmie <haniehghsmie@gmail.com>
2022-07-04 10:15:11 +0430

    Clean Admin controllers

--- OurSite.WebApi/Controllers/AdminController.cs
+++ OurSite.WebApi/Controllers/AdminController.cs
@@ -27,8 +27,10 @@ namespace OurSite.WebApi.Controllers
         }
         #endregion
 
+        #region Admin activities
+        #region login
         [HttpPost("login-Admin")]
-        public async Task<IActionResult> LoginAdmin([FromBody]ReqLoginDto reqLogin)
+        public async Task<IActionResult> LoginAdmin([FromBody] ReqLoginDto reqLogin)
         {
             if (ModelState.IsValid)
             {
@@ -43,59 +45,12 @@ namespace OurSite.WebApi.Controllers
             }
             return JsonStatusResponse.Error("مشکلی در اطلاعات ارسالی وجود دارد");
         }
+        #endregion
 
-        [Authorize(Roles = "General Manager,Admin")]
-        [HttpGet("change-user-status/{userId}")]
-        public async Task<IActionResult> ChangeUserStatus([FromRoute]long userId)
-        {
-            var res = await userService.ChangeUserStatus(userId);
-            if (res)
-                return JsonStatusResponse.Success("وضعیت کاربر تغییر کرد");
-            return JsonStatusResponse.Error("عملیات نا موفق بود");
-        }
-
-        [Authorize(Roles ="General Manager")]
-        [HttpPost("Update-Admin")]
-        public async Task<IActionResult> UpdateAdmin([FromBody]ReqUpdateAdminDto req)
-        {
-      
-                var res = await adminService.UpdateAdmin(req);
-                switch (res)
-                {
-                    case resUpdateAdmin.Success:
-                        return JsonStatusResponse.Success("با موفقیت ویرایش شد");
-                    case resUpdateAdmin.NotFound:
-                        return JsonStatusResponse.NotFound("حساب کاربری پیدا نشد");
-                    case resUpdateAdmin.Error:
-                        return JsonStatusResponse.Error("خطا در هنگام انجام عملیات");
-                    default:
-                        return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
-                }
-
-        }
-
-        [Authorize(Roles = "General Manager")]
-        [HttpDelete("delete-admin")]
-        public async Task<IActionResult> DeleteAdmin([FromQuery] long adminId)
-        {
-           var res= await adminService.DeleteAdmin(adminId);
-            if (res)
-                return JsonStatusResponse.Success("با موفقیت حذف شد");
-            return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
-        }
-        [Authorize(Roles = "General Manager")]
-        [HttpGet("view-admin/{adminId}")]
-        public async Task<IActionResult> GetAdmin([FromRoute]long adminId)
-        {
-            var res =await adminService.GetAdminById(adminId);
-            if (res != null)
-                return JsonStatusResponse.Success(res, "موفق");
-            HttpContext.Response.StatusCode = 404;
-            return JsonStatusResponse.NotFound("پیدا نشد");
-        }
+        #region singup
         [Authorize(Roles = "General Manager")]
         [HttpPost("register-admin")]
-        public async Task<IActionResult> RegisterAdmin([FromBody]ReqSingupUserDto req)
+        public async Task<IActionResult> RegisterAdmin([FromBody] ReqSingupUserDto req)
         {
             if (ModelState.IsValid)
             {
@@ -112,8 +67,31 @@ namespace OurSite.WebApi.Controllers
                 }
             }
             return JsonStatusResponse.Error("مشکلی در اطلاعات ارسالی وجود دارد");
-           
+
         }
+        #endregion
+
+        #region Update admin profile
+        [Authorize(Roles = "General Manager")]
+        [HttpPost("Update-Admin")]
+        public async Task<IActionResult> UpdateAdmin([FromBody] ReqUpdateAdminDto req)
+        {
+
+            var res = await adminService.UpdateAdmin(req);
+            switch (res)
+            {
+                case resUpdateAdmin.Success:
+                    return JsonStatusResponse.Success("با موفقیت ویرایش شد");
+                case resUpdateAdmin.NotFound:
+                    return JsonStatusResponse.NotFound("حساب کاربری پیدا نشد");
+                case resUpdateAdmin.Error:
+                    return JsonStatusResponse.Error("خطا در هنگام انجام عملیات");
+                default:
+                    return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
+            }
+
+        }
+        #endregion
 
         #region Reset Password
         [HttpPost("reset-password")]
@@ -127,6 +105,90 @@ namespace OurSite.WebApi.Controllers
                 return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
             }
             return JsonStatusResponse.Error("مشکلی در اطلاعات ارسالی وجود دارد");
+        }
+        #endregion
+
+        #endregion
+
+        #region Admin Management
+        [Authorize(Roles = "General Manager")]
+        [HttpDelete("delete-admin")]
+        public async Task<IActionResult> DeleteAdmin([FromQuery] long adminId)
+        {
+            var res = await adminService.DeleteAdmin(adminId);
+            if (res)
+                return JsonStatusResponse.Success("با موفقیت حذف شد");
+            return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
+        }
+        [Authorize(Roles = "General Manager")]
+        [HttpGet("view-admin/{adminId}")]
+        public async Task<IActionResult> GetAdmin([FromRoute] long adminId)
+        {
+            var res = await adminService.GetAdminById(adminId);
+            if (res != null)
+                return JsonStatusResponse.Success(res, "موفق");
+            HttpContext.Response.StatusCode = 404;
+            return JsonStatusResponse.NotFound("پیدا نشد");
+        }
+        #endregion
+
+        #region Role Management
+        [Authorize(Roles = "General Manager")] //add new role
+        [HttpPost("add-role")]
+        public async Task<IActionResult> AddRole([FromBody] RoleDto role)
+        {
+            var res = await roleService.AddRole(role);
+            if (res)
+                return JsonStatusResponse.Success("نقش با موفقیت اضافه شد");
+            return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
+        }
+
+        [Authorize(Roles = "General Manager")] //remove role
+        [HttpDelete("delete-role")]
+        public async Task<IActionResult> RemoveRole([FromBody] long RoleId)
+        {
+            var res = await roleService.RemoveRole(RoleId);
+            if (res)
+                return JsonStatusResponse.Success("نقش با موفقیت حذف شد");
+            return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
+        }
+
+        [Authorize(Roles = "General Manager")] //update role
+        [HttpPut("update-role")]
+        public async Task<IActionResult> UpdateRole(ReqUpdateRoleDto reqUpdate)
+        {
+            var res = await roleService.UpdateRole(reqUpdate);
+            switch (res)
+            {
+                case ReqUpdateRoleDto.ResUpdateRole.Success:
+                    return JsonStatusResponse.Success("نقش با موفقیت بروزرسانی شد");
+                case ReqUpdateRoleDto.ResUpdateRole.Error:
+                    return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
+                case ReqUpdateRoleDto.ResUpdateRole.NotFound:
+                    return JsonStatusResponse.NotFound("نقش مورد نظر پیدا نشد");
+                default:
+                    return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
+            }
+        }
+
+        [Authorize(Roles = "General Manager")] //Find role by id
+        [HttpGet("view-Role/{RoleId}")]
+        public async Task<IActionResult> GetRoleById([FromRoute] long RoleId)
+        {
+            var role = await roleService.GetRoleById(RoleId);
+            if (role is null)
+                return JsonStatusResponse.NotFound("نقش مورد نظر پیدا نشد");
+            return JsonStatusResponse.Success(role, "موفق");
+        }
+
+        [Authorize(Roles = "General Manager")] //find all role list
+        [HttpGet("view-Roles")]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var roles = await roleService.GetActiveRoles();
+            if (roles is null)
+                return JsonStatusResponse.NotFound("هیچ نقشی پیدا نشد");
+            return JsonStatusResponse.Success(roles, "موفق");
         }
         #endregion
 
@@ -149,88 +211,44 @@ namespace OurSite.WebApi.Controllers
         }
         #endregion
 
-        #region Role Management
-        [Authorize(Roles = "General Manager")]
-        [HttpPost("add-role")]
-        public async Task<IActionResult> AddRole([FromBody]RoleDto role)
-        {
-            var res =await roleService.AddRole(role);
-            if (res)
-                return JsonStatusResponse.Success("نقش با موفقیت اضافه شد");
-            return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
-        }
-
-        [Authorize(Roles = "General Manager")]
-        [HttpDelete("delete-role")]
-        public async Task<IActionResult> RemoveRole([FromBody]long RoleId)
-        {
-            var res =await roleService.RemoveRole(RoleId);
-            if(res)
-                return JsonStatusResponse.Success("نقش با موفقیت حذف شد");
-            return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
-        }
-
-        [Authorize(Roles = "General Manager")]
-        [HttpPut("update-role")]
-        public async Task<IActionResult> UpdateRole(ReqUpdateRoleDto reqUpdate)
-        {
-            var res = await roleService.UpdateRole(reqUpdate);
-            switch (res)
-            {
-                case ReqUpdateRoleDto.ResUpdateRole.Success:
-                    return JsonStatusResponse.Success("نقش با موفقیت بروزرسانی شد");
-                case ReqUpdateRoleDto.ResUpdateRole.Error:
-                    return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
-                case ReqUpdateRoleDto.ResUpdateRole.NotFound:
-                    return JsonStatusResponse.NotFound("نقش مورد نظر پیدا نشد");
-                default:
-                    return JsonStatusResponse.Error("عملیات با شکست مواجه شد");
-            }
-        }
-
-        [Authorize(Roles = "General Manager")]
-        [HttpGet("view-Role/{RoleId}")]
-        public async Task<IActionResult> GetRoleById([FromRoute]long RoleId)
-        {
-            var role = await roleService.GetRoleById(RoleId);
-            if (role is null)
-                return JsonStatusResponse.NotFound("نقش مورد نظر پیدا نشد");
-            return JsonStatusResponse.Success(role, "موفق");
-        }
-
-        [Authorize(Roles = "General Manager")]
-        [HttpGet("view-Roles")]
-        public async Task<IActionResult> GetAllRoles()
-        {
-            var roles = await roleService.GetActiveRoles();
-            if (roles is null)
-                return JsonStatusResponse.NotFound("هیچ نقشی پیدا نشد");
-            return JsonStatusResponse.Success(roles, "موفق");
-        }
-        #endregion
-        [HttpGet("view-user/{userid}")]
+        #region User management
+        [HttpGet("view-user/{userid}")] //view user by admin
         public async Task<IActionResult> ViewUser(long userid)
         {
-           var user = await userService.ViewUser(userid);
-           if (user is not null)
-                return JsonStatusResponse.Success(message:"success",ReturnData:user);
+            var user = await userService.ViewUser(userid);
+            if (user is not null)
+                return JsonStatusResponse.Success(message: "success", ReturnData: user);
 
 
             return JsonStatusResponse.Error(message: "کاربر پیدا نشد");
         }
 
 
-        [HttpGet("view-all-users")]
+        [HttpGet("view-all-users")] //Get user list
         public async Task<IActionResult> GetAllUser()
         {
             var users = await userService.GetAlluser();
             if (users.Any())
             {
-                return JsonStatusResponse.Success(message: "موفق" , ReturnData: users);
+                return JsonStatusResponse.Success(message: "موفق", ReturnData: users);
             }
             return JsonStatusResponse.NotFound(message: "کاربری یافت نشد");
-            
+
         }
+
+
+        
+        [Authorize(Roles = "General Manager,Admin")]
+        [HttpGet("change-user-status/{userId}")]
+        public async Task<IActionResult> ChangeUserStatus([FromRoute] long userId)
+        {
+            var res = await userService.ChangeUserStatus(userId);
+            if (res)
+                return JsonStatusResponse.Success("وضعیت کاربر تغییر کرد");
+            return JsonStatusResponse.Error("عملیات نا موفق بود");
+        }
+        #endregion
+
 
     }
 
