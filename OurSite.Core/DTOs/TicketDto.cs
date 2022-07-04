3e6d7beecdd2205634eb0f75c86cce528c5000b2
Erfan gharche beydokhti <erfan.erato.79@gmail.com>
2022-07-04 13:10:30 +0430

    Create Ticketing Dto's

---
+++ OurSite.Core/DTOs/TicketDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurSite.Core.DTOs
+{
+    public class TicketDto
+    {
+        public string TicketTitle { get; set; }
+        public string subject { get; set; }
+        public bool TicketSatate { get; set; }
+    }
+}
---
+++ OurSite.Core/DTOs/TicketMessageDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurSite.Core.DTOs
+{
+    public class TicketMessageDto
+    {
+        public string MessageText { get; set; }
+        public string? SubmittedTicketFileName { get; set; }
+        public bool IsSeen { get; set; }
+        public long UserId { get; set; }
+        public IFormFile? SubmittedTicketFile { get; set; }
+    }
+}
