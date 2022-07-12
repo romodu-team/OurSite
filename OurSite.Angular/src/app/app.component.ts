0ae89d95f598800a301351cfbe6ba180d0f275cc
Mr.mobin <barmar.mobin@gmail.com>
2022-07-12 13:24:28 +0430

    Bootstrap Updated

--- OurSite.Angular/src/app/app.component.ts
+++ OurSite.Angular/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { TestService } from './test.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'OurSite.Angular';
-
-  constructor(private testSer:TestService){}
-
-  ngOnInit(): void {
-    this.testSer.test()
-    
-  }
 }
