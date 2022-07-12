d1ddd9134cbe61b87c7dcb37c5a30d1f9ab8206c
faeze <faezeebold@gmail.com>
2022-07-12 13:44:11 +0430

    home added

--- OurSite.Angular/src/app/app.module.ts
+++ OurSite.Angular/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/home/header/header.component';
+import { ServicesComponent } from './pages/home/services/services.component';
+import { FormComponent } from './pages/home/form/form.component';
+import { Card1Component } from './shared/card1/card1.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ServicesComponent,
+    FormComponent,
+    Card1Component,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
