cf612065bc6c147172bed8dec8ba2deab1953438
hni <haniehghsmie@gmail.com>
2022-07-17 22:42:04 +0430

    Add New models for Feats Projects

---
+++ OurSite.Core/DTOs/ProjectDtos/EditProjectDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using OurSite.DataLayer.Entities.Projects;
+
+namespace OurSite.Core.DTOs.ProjectDtos
+{
+    public class EditProjectDto
+    {
+        public string Name { get; set; }
+        public ProType Type { get; set; }
+        public DateTime dateTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public situations Situation { get; set; }
+        public string PlanDetails { get; set; }
+        public long AdminId { get; set; }
+        public long UserId { get; set; }
+        public string ContractFileName { get; set; }
+
+
+
+    }
+
+
+}
+
