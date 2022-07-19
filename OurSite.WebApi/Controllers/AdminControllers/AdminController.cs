451d4e8ac6f45c20ba06625615977b4fb95dc139
sajjad ghorbani <sajjadr2001@gmail.com>
2022-07-19 13:30:59 +0430

    fix admin controller

--- OurSite.WebApi/Controllers/AdminControllers/AdminController.cs
+++ OurSite.WebApi/Controllers/AdminControllers/AdminController.cs
@@ -39,6 +39,7 @@ namespace OurSite.WebApi.Controllers
         [HttpPost("login-Admin")]
         public async Task<IActionResult> LoginAdmin([FromBody] ReqLoginDto reqLogin)
         {
+            AuthenticationHelper authenticationHelper = new AuthenticationHelper(roleService);
             if (ModelState.IsValid)
             {
                 var admin = await adminService.Login(reqLogin);
@@ -46,7 +47,7 @@ namespace OurSite.WebApi.Controllers
                     return JsonStatusResponse.NotFound("اطلاعات کاربری اشتباه است");
                 var role = await roleService.GetAdminRole(admin.Id);
 
-                var token = AuthenticationHelper.GenerateAdminToken(admin, role, 3);
+                var token = authenticationHelper.GenerateAdminToken(admin, role, 3);
                 HttpContext.Response.StatusCode = 200;
                 return JsonStatusResponse.Success(new { Token = token, Expire = 3, UserId = admin.Id, FirstName = admin.FirstName, LastName = admin.LastName }, "ورود با موفقیت انجام شد");
             }
