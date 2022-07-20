cd6b4f164343d6d5a8e4901c7f132e4bd225e8c7
faeze <faezeebold@gmail.com>
2022-07-20 12:24:24 +0430

    card6 edited

--- OurSite.Angular/src/app/shared/card6/card6.component.ts
+++ OurSite.Angular/src/app/shared/card6/card6.component.ts
@@ -12,8 +12,22 @@ export class Card6Component implements OnInit {
   ngOnInit(): void {
   }
 
-  @Input() img:string = '../../../assets/imgs/chart-histogram.png'
+  @Input() img:string = '../../../assets/imgs/UI@2x.ico'
   @Input() title:string = 'طراحی اختصاصی'
+
+  topHover:string = 'margin-top:0px'
+  cardHover:string = 'bg-white text-black'
+
+  CardHover(){
+    this.cardHover = 'bg-primary text-white'
+    this.topHover = 'margin-top:-3px'
+
+  }
+
+  CardInHover(){
+    this.cardHover = 'bg-white text-black'
+    this.topHover = 'margin-top:0px'
+  }
 
 
 
