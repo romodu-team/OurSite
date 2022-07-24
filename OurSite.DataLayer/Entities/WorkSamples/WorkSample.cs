7c059029fc46949b48dd3e63da26c7ca0c0074b2
sajjad ghorbani <sajjadr2001@gmail.com>
2022-07-24 20:53:48 +0430

    Update WorlSample Model

--- OurSite.DataLayer/Entities/WorkSamples/WorkSample.cs
+++ OurSite.DataLayer/Entities/WorkSamples/WorkSample.cs
@@ -2,6 +2,7 @@ using OurSite.DataLayer.Entities.BaseEntities;
 using OurSite.DataLayer.Entities.Comments;
 using OurSite.DataLayer.Entities.ImageGalleries;
 using OurSite.DataLayer.Entities.RatingModel;
+using System.Web.Mvc;
 
 namespace OurSite.DataLayer.Entities.WorkSamples;
 
@@ -10,10 +11,12 @@ public class WorkSample:BaseEntity
     public string ProjectName { get; set; }
     public string EmployerName { get; set; }
     public string CustomUrl { get; set; }
+    [AllowHtml]
     public string ShortDescription { get; set; }
     public string LogoImageName { get; set; }
     public string HeaderImageName { get; set; }
-    public string Description { get; set; }
+    [AllowHtml]
+    public string Content { get; set; }
 
 
 
