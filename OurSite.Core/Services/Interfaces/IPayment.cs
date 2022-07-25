0e401b1838603524adcff811743639384e15c5d1
hni <haniehghsmie@gmail.com>
2022-07-25 13:29:06 +0430

    Feat: interface pay

---
+++ OurSite.Core/Services/Interfaces/IPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OurSite.Core.DTOs.Payment;
+using static OurSite.Core.DTOs.ProjectDtos.CreatProjectDto;
+
+namespace OurSite.Core.Services.Interfaces.Projecta
+{
+    public interface IPayment : IDisposable
+    {
+        Task<ResProject> CreatePayment(CreatePaymentDto paydto, long AdminId);
+        Task<GetPayment> GetPayment(long PayId);
+        Task<ResFilterPayDto> GetAllProject(ReqFilterPayDto filter);
+
+
+
+
+
+    }
+}
+
--- OurSite.Core/Services/Interfaces/Projecta/IPayment.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace OurSite.Core.Services.Interfaces.Projecta
-{
-    public interface IPayment
-    {
-    }
-}
-
