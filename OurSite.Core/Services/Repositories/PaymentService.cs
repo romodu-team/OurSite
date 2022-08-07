52055eae6e1421d0184bfea1bace8e23f64ae837
hni <haniehghsmie@gmail.com>
2022-08-07 09:00:57 +0430

    Update PaymentService.cs

--- OurSite.Core/Services/Repositories/PaymentService.cs
+++ OurSite.Core/Services/Repositories/PaymentService.cs
@@ -191,7 +191,6 @@ namespace OurSite.Core.Services.Repositories
         #endregion
 
         #region Delete payment by admin
-        ]
         public async Task<ResProject> DeletePayment(long ProId, bool IsAdmin)
         {
             var pay = await PaymentRepositories.GetEntity(ProId);
