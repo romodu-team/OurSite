3ba10fb2599cf46ae6cd0c991000a9623c43f978
sajjad ghorbani <sajjadr2001@gmail.com>
2022-08-13 13:37:18 +0430

    Feat : Check box service and interface

--- OurSite.Core/Services/Interfaces/ICheckBoxService.cs
+++ OurSite.Core/Services/Interfaces/ICheckBoxService.cs
@@ -1,13 +1,16 @@
+using System.Collections.Specialized;
+using System;
 using OurSite.Core.DTOs.CheckBoxDtos;
 using OurSite.Core.DTOs.TicketDtos;
+using OurSite.DataLayer.Entities.ConsultationRequest;
 
 namespace OurSite.Core.Services.Interfaces;
 
-public interface ICheckBoxService
+public interface ICheckBoxService:IDisposable
 {
-        Task<bool> CreateCheckBox(string title,string? IconName, string? Description,int SiteSection);
-        Task<ResDeleteOpration> DeleteCheckBox(long priorityId);
-        Task<CheckBoxDto> GetCheckBox(long priorityId);
-        Task<bool> UpdateCheckBox(long priorityId, string? title, string? name);
+        Task<bool> CreateCheckBox(string title,string? IconName, string? Description,section SiteSection);
+        Task<ResDeleteOpration> DeleteCheckBox(long CheckBoxId);
+        Task<CheckBoxDto> GetCheckBox(long CheckBoxId);
+        Task<bool> UpdateCheckBox(long CheckBoxId, string title,string? IconName, string? Description,int? SiteSection);
         Task<List<CheckBoxDto>> GetAllCheckBox();
 }
---
+++ OurSite.Core/Services/Repositories/CheckBoxService.cs
@@ -0,0 +1,122 @@
+using System.Collections.Specialized;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+using OurSite.Core.DTOs.CheckBoxDtos;
+using OurSite.Core.DTOs.TicketDtos;
+using OurSite.Core.Services.Interfaces;
+using OurSite.DataLayer.Entities.ConsultationRequest;
+using OurSite.DataLayer.Entities.Projects;
+using OurSite.DataLayer.Interfaces;
+
+namespace OurSite.Core.Services.Repositories;
+
+public class CheckBoxService : ICheckBoxService
+{
+    #region Constructor
+    private IGenericReopsitories<CheckBoxs> _CheckBoxRepository;
+    private IGenericReopsitories<ItemSelected> _ConsultionItemSelectedRepository;
+    private IGenericReopsitories<SelectedProjectPlan> _SelectedProjectPlanRepository;
+    public CheckBoxService(IGenericReopsitories<ItemSelected> ConsultionItemSelectedRepository,IGenericReopsitories<SelectedProjectPlan> SelectedProjectPlanRepository,IGenericReopsitories<CheckBoxs> CheckBoxRepository)
+    {
+        _CheckBoxRepository=CheckBoxRepository;
+        _ConsultionItemSelectedRepository=ConsultionItemSelectedRepository;
+        _SelectedProjectPlanRepository=SelectedProjectPlanRepository;
+    }
+    #endregion
+    public async Task<bool> CreateCheckBox(string title, string? IconName, string? Description, section SiteSection)
+    {
+        if(!string.IsNullOrEmpty(title))
+            {
+                try
+                {
+                    await _CheckBoxRepository.AddEntity(new CheckBoxs {CheckBoxName=title,Description=Description,IconName=IconName,sectionName=SiteSection });
+                    await _CheckBoxRepository.SaveEntity();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+               
+            }
+            return false;
+    }
+
+    public async Task<ResDeleteOpration> DeleteCheckBox(long CheckBoxId)
+    {
+        //check if checkbox used in project and consultion form 
+        var Resprojects = await _SelectedProjectPlanRepository.GetAllEntity().Where(t => t.CheckBoxId == CheckBoxId && t.IsRemove == false).ToListAsync();
+        var consultions = await _ConsultionItemSelectedRepository.GetAllEntity().Where(t => t.CheckBoxId == CheckBoxId && t.IsRemove == false).ToListAsync();
+            if ((Resprojects != null & Resprojects.Count > 0)||(consultions != null & consultions.Count > 0))
+                return ResDeleteOpration.RefrenceError;
+            else
+            {
+                try
+                {
+                    var res = await _CheckBoxRepository.RealDeleteEntity(CheckBoxId);
+                    if (res)
+                    {
+                        await _CheckBoxRepository.SaveEntity();
+                        return ResDeleteOpration.Success;
+                    }
+                    return ResDeleteOpration.Failure;   
+                }
+                catch (Exception)
+                {
+
+                    return ResDeleteOpration.Failure;
+                }
+                
+            }
+    }
+
+    public void Dispose()
+    {
+        _CheckBoxRepository.Dispose();
+        _ConsultionItemSelectedRepository.Dispose();
+        _SelectedProjectPlanRepository.Dispose();
+    }
+
+    public async Task<List<CheckBoxDto>> GetAllCheckBox()
+    {
+        var CheckBoxs = await _CheckBoxRepository.GetAllEntity().Where(p => p.IsRemove == false).Select(p=>new CheckBoxDto {Title=p.CheckBoxName,Description=p.Description,IconName=p.IconName,Id=p.Id,SiteSectionName=p.sectionName == section.ConsultationForm?"فرم مشاوره":"پروژه"}).ToListAsync();
+            return CheckBoxs;
+    }
+
+    public async Task<CheckBoxDto> GetCheckBox(long CheckBoxId)
+    {
+         var CheckBox=await _CheckBoxRepository.GetEntity(CheckBoxId);
+            if (CheckBox == null)
+                return null;
+            return new CheckBoxDto { Title=CheckBox.CheckBoxName,Description=CheckBox.Description,IconName=CheckBox.IconName,Id=CheckBox.Id,SiteSectionName=CheckBox.sectionName == section.ConsultationForm?"فرم مشاوره":"پروژه" };
+    }
+
+    public async Task<bool> UpdateCheckBox(long CheckBoxId, string title, string? IconName, string? Description, int? SiteSection)
+    {
+        var CheckBox =await _CheckBoxRepository.GetEntity(CheckBoxId);
+            if(CheckBox != null)
+            {
+                if (!string.IsNullOrWhiteSpace(title))
+                    CheckBox.CheckBoxName = title;
+                if (!string.IsNullOrWhiteSpace(IconName))
+                    CheckBox.IconName = IconName;
+                if (!string.IsNullOrWhiteSpace(Description))
+                    CheckBox.Description = Description;
+                if (SiteSection != null)
+                    CheckBox.sectionName = (section)SiteSection;
+                try
+                {
+                    _CheckBoxRepository.UpDateEntity(CheckBox);
+                    await _CheckBoxRepository.SaveEntity();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+            return false;
+    }
+}
