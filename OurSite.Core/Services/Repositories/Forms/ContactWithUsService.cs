00d669b3701d014c815ac42fc1ccf30d59178b7a
sajjad ghorbani <sajjadr2001@gmail.com>
2022-08-13 09:46:19 +0430

    Feat: send email for contact us

--- OurSite.Core/Services/Repositories/Forms/ContactWithUsService.cs
+++ OurSite.Core/Services/Repositories/Forms/ContactWithUsService.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Security.Principal;
+using Microsoft.EntityFrameworkCore;
 using OurSite.Core.DTOs.ContactWithUsDtos;
+using OurSite.Core.DTOs.MailDtos;
 using OurSite.Core.DTOs.Paging;
 using OurSite.Core.Services.Interfaces;
+using OurSite.Core.Services.Interfaces.Mail;
 using OurSite.Core.Utilities.Extentions.Paging;
 using OurSite.DataLayer.Entities.ContactWithUs;
 using OurSite.DataLayer.Interfaces;
@@ -10,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace OurSite.Core.Services.Repositories.Forms
 {
@@ -17,9 +21,11 @@ namespace OurSite.Core.Services.Repositories.Forms
     {
         #region constructor
         private readonly IGenericReopsitories<ContactWithUs> contactWithUsRepo;
-        public ContactWithUsService(IGenericReopsitories<ContactWithUs> contactWithUsRepo)
+        private readonly IMailService mailService;
+        public ContactWithUsService(IGenericReopsitories<ContactWithUs> contactWithUsRepo,IMailService mailService)
         {
             this.contactWithUsRepo = contactWithUsRepo;
+            this.mailService=mailService;
         }
         #endregion
 
@@ -38,6 +44,12 @@ namespace OurSite.Core.Services.Repositories.Forms
             {
                 await contactWithUsRepo.AddEntity(form1);
                 await contactWithUsRepo.SaveEntity();
+                //send email for admin:you have new massage
+                var MailMessageDto= new MailRequestDto{
+                    ToEmail=form1.UserEmail,
+                    Subject="پیام تماس با ما جدید دریافت شد",
+                    Body=""};
+                await mailService.SendEmailAsync()
                 return true;
             }
             catch
@@ -55,7 +67,7 @@ namespace OurSite.Core.Services.Repositories.Forms
             var contactWithUsQuery = contactWithUsRepo.GetAllEntity();
             var count = (int)Math.Ceiling(contactWithUsQuery.Count() / (double)filter.TakeEntity);
             var pager = Pager.Build(count, filter.PageId, filter.TakeEntity);
-            var list = await contactWithUsRepo.GetAllEntity().Paging(pager).Where(u => u.IsRemove == false).Select(x => new GetAllContactWithUsDto { UserFirstName = x.UserFirstName, UserLastName = x.UserLastName, UserEmail = x.UserEmail, UserPhoneNumber = x.UserPhoneNumber }).ToListAsync();    //use the genric interface options and save values in variable
+            var list = await contactWithUsRepo.GetAllEntity().Paging(pager).Where(u => u.IsRemove == false).Select(x => new GetAllContactWithUsDto { Message=x.Expration,UserFirstName = x.UserFirstName, UserLastName = x.UserLastName, UserEmail = x.UserEmail, UserPhoneNumber = x.UserPhoneNumber }).ToListAsync();    //use the genric interface options and save values in variable
             var result = new ResFilterContactWithUsDto();
             result.SetPaging(pager);
             return result.SetContactWithUses(list);
@@ -68,6 +80,12 @@ namespace OurSite.Core.Services.Repositories.Forms
         public void Dispose()
         {
             contactWithUsRepo.Dispose();
+        }
+
+        public async Task<bool> AnswerToMessage(string ToEmail, string subject, string Content,List<IFormFile>? Attachments)
+        {
+           var res =await mailService.SendEmailAsync(new MailRequestDto(){ToEmail=ToEmail,Body=Content,Subject=subject,Attachments=Attachments});
+            return res;
         }
         #endregion
     }
