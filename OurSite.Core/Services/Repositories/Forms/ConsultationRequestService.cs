ff506499723afb2ae3cfaf1ac86a783c32616629
sajjad ghorbani <sajjadr2001@gmail.com>
2022-08-17 09:40:23 +0430

    Feat : Send Notification and Email in Services

--- OurSite.Core/Services/Repositories/Forms/ConsultationRequestService.cs
+++ OurSite.Core/Services/Repositories/Forms/ConsultationRequestService.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OurSite.Core.DTOs.ConsultationRequestDtos;
+using OurSite.Core.DTOs.MailDtos;
+using OurSite.Core.DTOs.NotificationDtos;
 using OurSite.Core.DTOs.Paging;
+using OurSite.Core.DTOs.UserDtos;
 using OurSite.Core.Services.Interfaces;
+using OurSite.Core.Services.Interfaces.Mail;
 using OurSite.Core.Utilities.Extentions.Paging;
 using OurSite.DataLayer.Entities.ConsultationRequest;
 using OurSite.DataLayer.Interfaces;
@@ -19,21 +23,29 @@ namespace OurSite.Core.Services.Repositories.Forms
         private readonly IGenericRepository<ConsultationRequest> consultationRequestRepo;
         private readonly IGenericRepository<CheckBoxs> checkBoxsRepo;
         private readonly IGenericRepository<ItemSelected> itemSelectedRepo;
-        public ConsultationRequestService(IGenericRepository<ItemSelected> itemSelectedRepo, IGenericRepository<CheckBoxs> checkBoxsRepo, IGenericRepository<ConsultationRequest> consultationRequestRepo)
+        private readonly INotificationService _notificationService;
+        private IAdminService _AdminService;
+        private readonly IMailService _mailService;
+
+        public ConsultationRequestService(IMailService mailService, IAdminService AdminService, INotificationService notificationService, IGenericRepository<ItemSelected> itemSelectedRepo, IGenericRepository<CheckBoxs> checkBoxsRepo, IGenericRepository<ConsultationRequest> consultationRequestRepo)
         {
+            _mailService = mailService;
             this.consultationRequestRepo = consultationRequestRepo;
             this.checkBoxsRepo = checkBoxsRepo;
             this.itemSelectedRepo = itemSelectedRepo;
+            _notificationService = notificationService;
+            _AdminService = AdminService;
         }
-        
-        
+
+
         #region Dispose
         public void Dispose()
         {
             consultationRequestRepo.Dispose();
             checkBoxsRepo.Dispose();
             itemSelectedRepo.Dispose();
-            
+            _notificationService.Dispose();
+
         }
 
 
@@ -41,7 +53,7 @@ namespace OurSite.Core.Services.Repositories.Forms
         #endregion
 
 
-        public async Task<bool> SendConsultationForm(ConsultationRequestDto consultationRequestDto,string? SubmittedFileName)
+        public async Task<bool> SendConsultationForm(ConsultationRequestDto consultationRequestDto, string? SubmittedFileName)
         {
             bool flag = true;
             ConsultationRequest request = new ConsultationRequest()
@@ -51,7 +63,7 @@ namespace OurSite.Core.Services.Repositories.Forms
                 UserPhoneNumber = consultationRequestDto.UserPhoneNumber,
                 Expration = consultationRequestDto.Expration,
                 SubmittedFileName = SubmittedFileName,
-                IsRead=false
+                IsRead = false
             };
             try
             {
@@ -64,8 +76,8 @@ namespace OurSite.Core.Services.Repositories.Forms
                 flag = false;
                 return flag;
             }
-           
-            if(consultationRequestDto.ItemSelectedsId is not null && consultationRequestDto.ItemSelectedsId.Count>0)
+
+            if (consultationRequestDto.ItemSelectedsId is not null && consultationRequestDto.ItemSelectedsId.Count > 0)
             {
                 var list = consultationRequestDto.ItemSelectedsId[0].Split(",");
                 foreach (var item in list)
@@ -77,25 +89,59 @@ namespace OurSite.Core.Services.Repositories.Forms
                         {
                             CheckBoxId = Convert.ToInt64(item),
                             ConsultationFormId = request.Id
-                            
+
                         };
                         await itemSelectedRepo.AddEntity(selectedItem);
                     }
-     
+
                 }
-            try
+                try
+                {
+                    await itemSelectedRepo.SaveEntity();
+
+                    flag = true;
+                    //send notification to admins
+                    var Adminaccounts = await _AdminService.GetAllAdmin(new ReqFilterUserDto { PageId = 1, TakeEntity = 1000 });
+                    if (Adminaccounts.Admins != null && Adminaccounts.Admins.Count > 0)
+                    {
+                        foreach (var admin in Adminaccounts.Admins)
+                        {
+                            await _notificationService.CreateNotification(new ReqCreateNotificationDto { AccountUUID = admin.UUID, Message = $"فرم درخواست مشاوره جدید دریافت شد." });
+                        }
+                    }
+                    //send email to support
+                    var MailMessage = new MailRequestDto
+                    {
+                        ToEmail = "Support@romodu.com",
+                        Subject = "پیام تماس با ما جدید دریافت شد",
+                        Body = $"متن پیام : فرم درخواست مشاوره جدید دریافت شد."
+                    };
+                    await _mailService.SendEmailAsync(MailMessage);
+                    return flag;
+                }
+                catch
+                {
+                    flag = false;
+                    return flag;
+                }
+            }
+            //send notification to admins
+            var accounts = await _AdminService.GetAllAdmin(new ReqFilterUserDto { PageId = 1, TakeEntity = 1000 });
+            if (accounts.Admins != null && accounts.Admins.Count > 0)
             {
-                await itemSelectedRepo.SaveEntity();
-                
-                flag= true;
-                return flag;
+                foreach (var admin in accounts.Admins)
+                {
+                    await _notificationService.CreateNotification(new ReqCreateNotificationDto { AccountUUID = admin.UUID, Message = $"فرم درخواست مشاوره جدید دریافت شد." });
+                }
             }
-            catch
+            //send email to support
+            var MailMessageDto = new MailRequestDto
             {
-                flag= false;
-                return flag;
-            }
-            }
+                ToEmail = "Support@romodu.com",
+                Subject = "پیام تماس با ما جدید دریافت شد",
+                Body = $"متن پیام : فرم درخواست مشاوره جدید دریافت شد."
+            };
+            await _mailService.SendEmailAsync(MailMessageDto);
             return flag;
         }
 
@@ -109,7 +155,7 @@ namespace OurSite.Core.Services.Repositories.Forms
             var consultationRequestQuery = consultationRequestRepo.GetAllEntity();
             var count = (int)Math.Ceiling(consultationRequestQuery.Count() / (double)filter.TakeEntity);
             var pager = Pager.Build(count, filter.PageId, filter.TakeEntity);
-            var list = await consultationRequestRepo.GetAllEntity().Paging(pager).Where(u => u.IsRemove == false).Select(x => new GetAllConsultationRequestDto {IsRead=x.IsRead,Id=x.Id, UserFullName = x.UserFullName, UserEmail = x.UserEmail, UserPhoneNumber = x.UserPhoneNumber }).ToListAsync();
+            var list = await consultationRequestRepo.GetAllEntity().Paging(pager).Where(u => u.IsRemove == false).Select(x => new GetAllConsultationRequestDto { IsRead = x.IsRead, Id = x.Id, UserFullName = x.UserFullName, UserEmail = x.UserEmail, UserPhoneNumber = x.UserPhoneNumber }).ToListAsync();
             var result = new ResFilterConsultationRequestDto();
             result.SetPaging(pager);
             return result.SetConsultationRequests(list);
@@ -117,26 +163,27 @@ namespace OurSite.Core.Services.Repositories.Forms
         }
 
         #endregion
-        
+
         #region Get Consulation From
         public async Task<GetConsulationFormDto> GetConsulationForm(long ConsultationFormId)
         {
-            var consulationFrom = await consultationRequestRepo.GetAllEntity().Include(c=>c.ItemSelecteds).ThenInclude(c=>c.CheckBox).SingleOrDefaultAsync(c=>c.Id==ConsultationFormId);
-            if(consulationFrom is not null)
+            var consulationFrom = await consultationRequestRepo.GetAllEntity().Include(c => c.ItemSelecteds).ThenInclude(c => c.CheckBox).SingleOrDefaultAsync(c => c.Id == ConsultationFormId);
+            if (consulationFrom is not null)
             {
-                var dto = new GetConsulationFormDto(){
-                    Expration=consulationFrom.Expration,
-                    CreateDate=consulationFrom.CreateDate.ToString(),
-                    LastUpdateDate=consulationFrom.LastUpdate.ToString(),
-                    UserEmail=consulationFrom.UserEmail,
-                    UserFullName=consulationFrom.UserFullName,
-                    UserPhoneNumber=consulationFrom.UserPhoneNumber,
-                    ItemSelected=new List<DTOs.CheckBoxDtos.CheckBoxDto?>(),
-                    IsRead=consulationFrom.IsRead
+                var dto = new GetConsulationFormDto()
+                {
+                    Expration = consulationFrom.Expration,
+                    CreateDate = consulationFrom.CreateDate.ToString(),
+                    LastUpdateDate = consulationFrom.LastUpdate.ToString(),
+                    UserEmail = consulationFrom.UserEmail,
+                    UserFullName = consulationFrom.UserFullName,
+                    UserPhoneNumber = consulationFrom.UserPhoneNumber,
+                    ItemSelected = new List<DTOs.CheckBoxDtos.CheckBoxDto?>(),
+                    IsRead = consulationFrom.IsRead
                 };
                 foreach (var item in consulationFrom.ItemSelecteds)
                 {
-                    var checkbox = new DTOs.CheckBoxDtos.CheckBoxDto{Description=item.CheckBox.Description,IconName=item.CheckBox.IconName,Title=item.CheckBox.CheckBoxName,Id=item.CheckBox.Id};
+                    var checkbox = new DTOs.CheckBoxDtos.CheckBoxDto { Description = item.CheckBox.Description, IconName = item.CheckBox.IconName, Title = item.CheckBox.CheckBoxName, Id = item.CheckBox.Id };
                     dto.ItemSelected.Add(checkbox);
                 }
 
@@ -147,8 +194,8 @@ namespace OurSite.Core.Services.Repositories.Forms
         #endregion
         public async Task<bool> ChangeReadStatus(long ConsultationFormId)
         {
-            var form= await consultationRequestRepo.GetEntity(ConsultationFormId);
-            form.IsRead=!form.IsRead;
+            var form = await consultationRequestRepo.GetEntity(ConsultationFormId);
+            form.IsRead = !form.IsRead;
             try
             {
                 consultationRequestRepo.UpDateEntity(form);
