8992ee3fa539523731e2590db5ac1e448a1c3072
hni <haniehghsmie@gmail.com>
2022-08-17 09:07:27 +0430

    Feat: Creat Persian and Ad date extension

---
+++ OurSite.Core/Utilities/Extentions/PersianCalendarExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace OurSite.Core.Utilities.Extentions
+{
+    public static class PersianCalendarExtension
+    {
+        public static string PersianDate(this DateTime datetime)
+        {
+
+                PersianCalendar pc = new PersianCalendar();
+                string date = $"{pc.GetYear((DateTime)datetime)}/{pc.GetMonth((DateTime)datetime)}/{pc.GetDayOfMonth((DateTime)datetime)}";
+                return date;
+
+                
+        }
+
+        public static DateTime AdDate(this string PersianDate)
+        {
+
+                PersianCalendar pc = new PersianCalendar();
+                var date = pc.ToDateTime(1401, 01, 01, 0 , 0 , 0 , 0);
+                return date;
+
+        }
+    }
+}
+
