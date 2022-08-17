b7a49468a1a0494a1770901be4139fbd41cde1ac
sajjad ghorbani <sajjadr2001@gmail.com>
2022-08-17 13:30:35 +0430

    Fix : Mobile Reqular Expression

--- OurSite.Core/DTOs/AdminDtos/ReqRegisterAdminDto.cs
+++ OurSite.Core/DTOs/AdminDtos/ReqRegisterAdminDto.cs
@@ -11,6 +11,7 @@ namespace OurSite.Core.DTOs.AdminDtos
         public string Password { get; set; }
         [Compare("Password", ErrorMessage = "رمز‌های عبور با یکدیگر مغایرت ندارند")]
         public string ConmfrimPassword { get; set; }
+        [RegularExpression("09(1[0-9]|3[1-9]|2[0-9]|9[0-9]|0[1-9]|4[1-9])-?[0-9]{3}-?[0-9]{4}",ErrorMessage = "شماره تماس وارد شده صحیح نیست")]
         public string Mobile { get; set; }
         public string Email { get; set; }
         //   public singup Singup { get; set; }
