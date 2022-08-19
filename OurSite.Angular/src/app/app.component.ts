606ae5c27189f44585057b648bf88338e6399744
Mr.mobin <barmar.mobin@gmail.com>
2022-08-19 13:47:31 +0430

    auth bug fixed

--- OurSite.Angular/src/app/app.component.ts
+++ OurSite.Angular/src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'OurSite.Angular';
+export class AppComponent implements OnInit{
+  
+  constructor(private authSer:AuthService) {}
+  ngOnInit(): void {
+      this.checkAuth()
+  }
+
+  checkAuth(){
+    if(localStorage.getItem('userToken')){
+      this.authSer.userLoggedIn = true
+    }else{
+      this.authSer.userLoggedIn = false
+    }
+  }
+
 }
