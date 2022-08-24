0785efe2cccb7f5e160d72a17906a758744d7ac4
Mr.mobin <barmar.mobin@gmail.com>
2022-08-24 12:25:38 +0430

    adminUser header added

--- OurSite.Angular/src/app/shared/adminuser-header/adminuser-header.component.ts
+++ OurSite.Angular/src/app/shared/adminuser-header/adminuser-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-adminuser-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminuserHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authSer:AuthService) { }
 
   ngOnInit(): void {
   }
