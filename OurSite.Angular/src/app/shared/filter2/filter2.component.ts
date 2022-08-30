410e0b5815575e29fcfc1adeda96b2601b24a294
sina joon <sinapourjalal18@gmail.com>
2022-08-30 12:42:17 +0430

    filter2 edited

---
+++ OurSite.Angular/src/app/shared/filter2/filter2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter2',
+  templateUrl: './filter2.component.html',
+  styleUrls: ['./filter2.component.css']
+})
+export class Filter2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
